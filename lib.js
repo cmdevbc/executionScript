0e7ef27a75709ee551e4efac70c0844371b8439d
cmdevbc <cmdevbc@gmail.com>
2022-03-16 16:49:52 +0300

    add experimental nonce management

--- lib.js
+++ lib.js
@@ -27,6 +27,7 @@ const web3s = {};
 const priceCache = {};
 const rpcCache = {};
 const timerSyncCache = {};
+const nonces = {};
 
 const priceData = {};
 
@@ -481,8 +482,27 @@ const chooseRpc = async (network) => {
 
 const getNonce = async (pid) => {
   const predictionData = predictions[pid];
-  const nonce = await signers[predictionData.network].getTransactionCount();
-  console.log('transaction count:', nonce);
+  let nonce = await signers[predictionData.network].getTransactionCount();
+
+  if(nonce.latest){
+    if(nonce > nonce.latest.nonce){
+      nonces.latest = {nonce, id:pid};
+    }
+    else {
+      if(nonce.latest.id == pid){
+        console.log('nonce:', nonce);
+        return nonce.latest.nonce;
+      }
+      
+      if(nonces.latest.id != pid){
+        nonce++;
+      }
+    }
+  }
+
+  nonces.latest = {nonce, id:pid};
+  
+  console.log('nonce:', nonce);
   return nonce;
 }
 
