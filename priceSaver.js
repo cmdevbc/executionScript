063cb09cd45338452b4d91ec463fb8ca3dc5856a
cmdevbc <cmdevbc@gmail.com>
2022-04-26 15:22:41 +0300

    use huobi price

--- priceSaver.js
+++ priceSaver.js
@@ -7,6 +7,7 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const priceData = {};
+let binanceTimeSync = 0;
 
 const getFileName = (prediction) => {
   return (
@@ -33,6 +34,41 @@ const getPriceKUCOIN = async (code) => {
   }
 };
 
+const getPriceHUOBI = async (code) => {
+  try {
+    const data = await fetch(
+      `https://api.huobi.pro/market/trade?symbol=${code}`
+    );
+    const dataJson = await data.json();
+    if (!dataJson || !dataJson.tick) return 0;
+    const price = dataJson.tick.data[0].price;
+    const timestamp = Math.round(dataJson.tick.data[0].ts / 1000);
+    const result =  { timestamp, price };
+    return result;
+  } catch (err) {
+    return null;
+  }
+};
+
+const getPriceBINANCE = async (code) => {
+  try {
+    const data = await fetch(
+      "https://api.binance.com/api/v3/ticker/price?symbol=" + code
+    );
+    const dataJson = await data.json();
+    const price = dataJson.price;
+    const priceTemp = Math.round(parseFloat(price) * 100000000);
+
+    const timestamp = Math.round(Date.now() / 1000) + binanceTimeSync;
+
+    const result = { timestamp, price: priceTemp };
+    console.log("cem binance result", result);
+    return result;
+  } catch (err) {
+    return null;
+  }
+};
+
 const loadPriceDataToCache = (prediction) => {
   const fileName = getFileName(prediction);
   if (!fs.existsSync(fileName)) {
@@ -51,7 +87,15 @@ const savePrice = async (prediction, priceArrLength) => {
   }
 
   if (!prediction.keepPaused) {
-    const data = await getPriceKUCOIN(prediction.apicode);
+    let data;
+
+    if (prediction.apitype == "KUCOIN")
+      data = await getPriceKUCOIN(prediction.apicode);
+    else if (prediction.apitype == "BINANCE")
+      data = await getPriceBINANCE(prediction.apicode);
+    else if (prediction.apitype == "HUOBI")
+      data = await getPriceHUOBI(prediction.apicode);
+
     if (data) {
       const fileName = getFileName(prediction);
 
@@ -66,14 +110,25 @@ const savePrice = async (prediction, priceArrLength) => {
   }
 };
 
-const checkPredictions = () => {
+const checkPredictions = async () => {
   for (let i = 0; i < config.predictions.length; i++) {
     const prediction = config.predictions[i];
-    if (prediction.apitype == "KUCOIN") {
+    if (!prediction.skipSavePrice) {
+      if (prediction.apitype == "BINANCE") {
+        const timeData = await fetch("https://api.binance.com/api/v3/time");
+        const timeDataJson = await timeData.json();
+        binanceTimeSync =
+          Math.round(timeDataJson.serverTime / 1000) -
+          Math.round(Date.now() / 1000);
+        console.log("BINANCE time sync is", binanceTimeSync);
+      }
 
       const saveInterval = prediction.saveInterval || 500;
-      const priceArrLength = 10 + prediction.interval * 1000 / saveInterval;
-      setInterval(() => savePrice(prediction, priceArrLength), prediction.saveInterval);
+      const priceArrLength = 10 + (prediction.interval * 1000) / saveInterval;
+      setInterval(
+        () => savePrice(prediction, priceArrLength),
+        prediction.saveInterval
+      );
     }
   }
 };
