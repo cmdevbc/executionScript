bcbc85f4b0161febc5913b392bdf992ad4809025
cmdevbc <cmdevbc@gmail.com>
2022-04-26 16:12:18 +0300

    use gas +10

--- globalConfig.js
+++ globalConfig.js
@@ -14,7 +14,7 @@ const config = {
         },
         POLYGON: {
             gasLevel:'FastGasPrice',
-            gasOffset:1,
+            gasOffset:10,
             gasPerIncrement:10,
             rpcOptions: ["https://speedy-nodes-nyc.moralis.io/38d762dc7ea8dc00bd74ca7a/polygon/mainnet", "https://polygon-rpc.com/", "https://matic-mainnet.chainstacklabs.com"],
             checkGas: true,
